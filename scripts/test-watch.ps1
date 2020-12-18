a1afb0440a094a0a1cd1dfc2af6fea1a7cb89ce5
Rizart Dokollari <r.dokollari@gmail.com>
2020-12-18 13:07:23 +0000

    Remove python version number for test script.

--- scripts/test-watch.ps1
+++ scripts/test-watch.ps1
@@ -4,8 +4,8 @@ $RootDir = "${ScriptDir}/.."
 Write-Output "Output some message"
 
 $testWatch = "cd ${RootDir}; " `
-    + "python3 -m venv .venv; " `
-    + "pip3 install -r requirements.lock; " `
+    + "python -m venv .venv; " `
+    + "pip install -r requirements.lock; " `
     + ". ${RootDir}\.venv\Scripts\Activate.ps1; " `
     + "pytest-watch"
 
--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,8 +2,8 @@ $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $RootDir = "${ScriptDir}/.."
 
 $testWatch = "cd ${RootDir}; " `
-    + "python3 -m venv .venv; " `
-    + "pip3 install -r requirements.lock; " `
+    + "python -m venv .venv; " `
+    + "pip install -r requirements.lock; " `
     + ". ${RootDir}\.venv\Scripts\Activate.ps1; " `
     + "pytest"
 
