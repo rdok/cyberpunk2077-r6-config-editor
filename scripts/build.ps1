acf6961f4f20c2b8187ea135a50dba66d39ce30f
Rizart Dokollari <r.dokollari@gmail.com>
2020-12-23 19:34:54 +0000

    Set minimum crafting speed from 0.1 to 0.01.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,12 +1,3 @@
-param (
-    [Parameter(Mandatory = $true)][string]$env
-)
-
-If ($env -ne 'prod' -And $env -ne 'dev')
-{
-    throw "Invalid env. Should be dev or prod."
-}
-
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 . ${ScriptDir}\setup-venv.ps1 -env prod
 
