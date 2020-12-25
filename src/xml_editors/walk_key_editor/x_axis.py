c154fd165f1302b61131222392fb9dc155648736
Rizart Dokollari <r.dokollari@gmail.com>
2020-12-25 13:43:25 +0000

    Move x axis forward/back under a single file.

--- src/xml_editors/walk_key_editor/x_axis.py
+++ src/xml_editors/walk_key_editor/x_axis.py
@@ -2,15 +2,27 @@ from xml.etree import ElementTree
 from xml.etree.ElementTree import SubElement
 
 from src.xml_editors.IDLocators import IDLocators
-from src.xml_editors.walk_key_editor.x_axis import XAxis
 
 
-class XAxisBack(XAxis):
+class XAxis:
+    x_axis_xpath = './/mapping[@name="LeftX_Axis"]'
+
     def __init__(self, id_locators: IDLocators):
         super().__init__()
         self.id_locators = id_locators
 
-    def put(self, root: ElementTree):
+    def put_forward(self, root: ElementTree):
+        x_axis = root.find(self.x_axis_xpath)
+        forward = x_axis.find('.//button[@overridableUI="forward"]')
+
+        if forward is None:
+            id = self.id_locators.forward(root)
+            attributes = {'id': id, 'val': '0', 'overridableUI': 'forward'}
+            SubElement(x_axis, 'button', attributes)
+        else:
+            forward.set('val', '0')
+
+    def put_back(self, root):
         x_axis = root.find(self.x_axis_xpath)
         back = x_axis.find('.//button[@overridableUI="back"]')
 
